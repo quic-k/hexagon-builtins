09e13b77d8786ff33d50cdd59714dea8a36eab4d
Kushal Pal <kushpal@qti.qualcomm.com>
2025-07-31 16:33:01 +0530

    Add all configs in one file
    Signed-off-by: Kushal Pal <kushpal@qti.qualcomm.com>

--- hexagon-builtins-build.sh
+++ hexagon-builtins-build.sh
@@ -14,19 +14,10 @@ TOOLCHAIN_INSTALL=$(realpath $1)
 echo TOOLCHAIN_INSTALL : $TOOLCHAIN_INSTALL
 PATH="$TOOLCHAIN_INSTALL/bin/:$PATH" \
                   cmake -G Ninja \
-                  -DCMAKE_C_FLAGS="-ffreestanding" \
-                  -DCMAKE_CXX_FLAGS="-ffreestanding" \
                   -DCMAKE_BUILD_TYPE=Release \
                   -DLLVM_CMAKE_DIR:PATH=$TOOLCHAIN_INSTALL \
                   -DCMAKE_INSTALL_PREFIX:PATH=$($TOOLCHAIN_INSTALL/bin/clang -print-resource-dir) \
-                  -DCMAKE_CROSSCOMPILING:BOOL=ON \
-                  -DCOMPILER_RT_OS_DIR=baremetal \
-                  -DCAN_TARGET_hexagon=1 \
-                  -DCAN_TARGET_x86_64=0 \
-                  -DCMAKE_C_COMPILER_FORCED:BOOL=ON \
-                  -DCMAKE_CXX_COMPILER_FORCED:BOOL=ON \
                   -C ./hexagon-builtins.cmake \
-                  -C ./hexagon-cross.cmake \
                   -B ./ \
                   -S ../compiler-rt
 
